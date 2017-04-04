42239993a677eb0da3b4cc63cd61554ca2e0c52a
jackm <jack@ehelion.com>
2017-04-04 17:03:41 -0400

    Added multiline ad description support and cleaned up question prompts so that they are easier to understand

--- GenerateInfFile.py
+++ GenerateInfFile.py
@@ -4,20 +4,18 @@ import requests
 import json
 from PostingCategory import *
 adType = ['OFFER', 'WANTED']
-priceType = ['FIXED','GIVE_AWAY', 'CONTACT', 'SWAP_TRADE']
-
+priceType = ['FIXED', 'GIVE_AWAY', 'CONTACT', 'SWAP_TRADE']
 
 
 #Dictionary w/ postal_code, lat, lng, city, province
 def getAddressMap():
-    address = input("What is your address?")
-    data={'address': address}
+    address = input("Your address: ")
+    data = {'address': address}
     endpoint = 'https://maps.googleapis.com/maps/api/geocode/json'
     resp = requests.get(endpoint,params=data)
 
     ans = {}
     latlng = json.loads(resp.text)['results'][0]['geometry']['location']
-
     ans['lat'] = str(latlng['lat'])
     ans['lng'] = str(latlng['lng'])
     postalCode = [item for item in json.loads(resp.text)['results'][0]['address_components'] if "postal_code" in item['types'] ][0]['short_name']
@@ -29,39 +27,59 @@ def getAddressMap():
     return ans
 
 def getEnum(array):
-    print("Which of the following pertains most to you?")
     for i, item in enumerate(array):
-        print(i+1, ".", item)
-    response = int(input())
+        print("{:>2d} - {}".format(i+1, item))
+    response = int(input("Choose one: "))
     return array[response-1]
 
 #{'category': catId, 'attirbute: 'attrid', 'attribute': 'attrid''}}
 def pickCategory():
     ans = {}
-    keyword = input("Please give a keyword for your category")
-    possibleCategories = sqliteSession.query(PostingCategory).filter(PostingCategory.name.like("%"+keyword+"%"))
+    while True:
+        keyword = input("Provide a category keyword to search for: ")
+        possibleCategories = sqliteSession.query(PostingCategory).filter(PostingCategory.name.like("%"+keyword+"%"))
+        if possibleCategories.count() < 1:
+            print("Could not find any categories using the given keyword. Try again.")
+        else:
+            break
     for i, cat in enumerate(possibleCategories):
-        print(i+1, ". ", cat)
-    print("Which category is most related?")
-    response = int(input())
+        print("{:>2d} - {}".format(i+1, cat))
+    response = int(input("Select a category from the list above (choose number): "))
     selectedCategory = possibleCategories[response-1]
     ans['category'] = selectedCategory.kijijiId
     for attribute in selectedCategory.attribute:
         for i, attrValue in enumerate(attribute.acceptableValue):
             print(i+1, attrValue.value)
-        response = int(input("Which one is most relevant relating to " + attribute.kijijiName + "?"))
+        response = int(input("Choose most relevant category relating to " + attribute.kijijiName + ": "))
         ans[attribute.kijijiName] = attribute.acceptableValue[response-1].kijijiValue
     return ans
 
+# Multiline ad description
+def getDescription():
+    contents = []
+    print("Enter multiline ad description. Type 'EOF' on a new line to finish.")
+    while True:
+        line = input()
+        if line == "EOF":
+            break
+        contents.append(line)
+    return "\\n".join(contents)
+
+
 categoryMap = pickCategory()
 addressMap = getAddressMap()
-title = input("What's the title of your item?")
-description = input("What's the description of your item?")
+# TODO: Figure out a way to determine appropriate location ID and location area ID from geolocation coords
+locationId = "1700212"
+locationArea = "1700209"
+title = input("Ad title: ")
+description = getDescription()
+print("Ad price type:")
 pmtType = getEnum(priceType)
 if pmtType == 'FIXED':
-    price = input("How much are you selling for?")
+    price = input("Ad price in dollars: ")
+print("Ad type:")
 ad = getEnum(adType)
-photos = input("Give me the names of your photos, separated by a comma")
+photos = input("List of image filenames to upload (comma separated): ")
 
 f = open('myAd.inf', 'w')
 f.write("postAdForm.geocodeLat="+addressMap['lat']+"\n")
@@ -79,8 +97,8 @@ if pmtType == 'FIXED':
 f.write("postAdForm.attributeMap[forsaleby_s]=ownr"+"\n")
 f.write("postAdForm.title="+title+"\n")
 f.write("postAdForm.description="+description+"\n")
-f.write("postAdForm.locationId=1700212"+"\n") #TODO: Get this info from user
-f.write("locationLevel0=1700209"+"\n")
+f.write("postAdForm.locationId="+locationId+"\n")
+f.write("locationLevel0="+locationArea+"\n")
 f.write("postAdForm.postalCode="+addressMap['postal_code']+"\n")
 f.write("featuresForm.topAdDuration=7"+"\n")
 f.write("submitType=saveAndCheckout"+"\n")
