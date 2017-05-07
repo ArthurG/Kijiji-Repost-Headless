0ea5943301049e9a03666794da11666f4ddd629f
jackm <jack@ehelion.com>
2017-05-07 14:58:35 -0400

    Minor syntax and spacing cleanup.

--- KijijiApi.py
+++ KijijiApi.py
@@ -13,20 +13,20 @@ class KijijiApiException(Exception):
         if dump:
             with open('/tmp/kijiji-api-dump', 'w') as dumpfile:
                 dumpfile.write(dump)
-    def __str(self):
-        return 'View /tmp/kijiji-api-dump/ for last dumpfile'
+    def __str__(self):
+        return "View /tmp/kijiji-api-dump/ for last dumpfile"
 
 class SignInException(KijijiApiException):
     def __str__(self):
-        return 'Could not sign in.\n'+super().__str__()
+        return "Could not sign in.\n"+super().__str__()
 
 class PostAdException(KijijiApiException):
     def __str__(self):
-        return 'Could not post ad.\n'+super().__str__()
+        return "Could not post ad.\n"+super().__str__()
 
 class DeleteAdException(KijijiApiException):
     def __str__(self):
-        return 'Could not delete ad.\n'+super().__str__()
+        return "Could not delete ad.\n"+super().__str__()
 
 
 #Retrive CSRF token from webpage
@@ -42,7 +42,7 @@ def uploadOneImage(imgFile):
     for i in range(0, 3):
         files = {'file': imgFile}
         ses = requests.Session()
-        r = ses.post(imageUploadUrl, files = files)
+        r = ses.post(imageUploadUrl, files=files)
         if (r.status_code != 200):
             raise PostAdException(r.text)
         try:
@@ -53,12 +53,11 @@ def uploadOneImage(imgFile):
         except KeyError as e:
             print("Image Upload failed on try #{}".format(i+1))
             pass
-    return
-
+    return ""
 
 
 class KijijiApi:
-    #login:  user, password -> None (Sets session to logged in)
+    #login: user, password -> None (Sets session to logged in)
     #isLoggedIn: None -> bool
     #All function requires a logged in session to function correctly
     #logout: None -> None
@@ -74,44 +73,42 @@ class KijijiApi:
     def login(self, username, password):
         loginUrl = 'https://www.kijiji.ca/t-login.html'
         resp = self.session.get(loginUrl)
-
         payload = {
-                'emailOrNickname': username,
-                'password': password,
-                'rememberMe': 'true',
-                '_rememberMe': 'on',
-                'ca.kijiji.xsrf.token': getToken(resp.text, 'ca.kijiji.xsrf.token'),
-                'targetUrl': 'L3QtbG9naW4uaHRtbD90YXJnZXRVcmw9TDNRdGJHOW5hVzR1YUhSdGJEOTBZWEpuWlhSVmNtdzlUREpuZEZwWFVuUmlNalV3WWpJMGRGbFlTbXhaVXpoNFRucEJkMDFxUVhsWWJVMTZZbFZLU1dGVmJHdGtiVTVzVlcxa1VWSkZPV0ZVUmtWNlUyMWpPVkJSTFMxZVRITTBVMk5wVW5wbVRHRlFRVUZwTDNKSGNtVk9kejA5XnpvMnFzNmc2NWZlOWF1T1BKMmRybEE9PQ--'
-                }
-        resp = self.session.post(loginUrl, data = payload)
+            'emailOrNickname': username,
+            'password': password,
+            'rememberMe': 'true',
+            '_rememberMe': 'on',
+            'ca.kijiji.xsrf.token': getToken(resp.text, 'ca.kijiji.xsrf.token'),
+            'targetUrl': 'L3QtbG9naW4uaHRtbD90YXJnZXRVcmw9TDNRdGJHOW5hVzR1YUhSdGJEOTBZWEpuWlhSVmNtdzlUREpuZEZwWFVuUmlNalV3WWpJMGRGbFlTbXhaVXpoNFRucEJkMDFxUVhsWWJVMTZZbFZLU1dGVmJHdGtiVTVzVlcxa1VWSkZPV0ZVUmtWNlUyMWpPVkJSTFMxZVRITTBVMk5wVW5wbVRHRlFRVUZwTDNKSGNtVk9kejA5XnpvMnFzNmc2NWZlOWF1T1BKMmRybEE9PQ--'
+            }
+        resp = self.session.post(loginUrl, data=payload)
         if not self.isLoggedIn():
             raise SignInException(resp.text)
 
     def isLoggedIn(self):
         indexPageText = self.session.get('https://www.kijiji.ca/m-my-ads.html/').text
-        return 'Sign Out' in indexPageText 
+        return "Sign Out" in indexPageText 
 
     def logout(self):
         self.session.get('https://www.kijiji.ca/m-logout.html')
 
     def deleteAd(self, adId):
         myAdsPage = self.session.get('https://www.kijiji.ca/m-my-ads.html')
-
         params = {
-                'Action': 'DELETE_ADS',
-                'Mode': 'ACTIVE',
-                'needsRedirect': 'false',
-                'ads': '[{{"adId":"{}","reason":"PREFER_NOT_TO_SAY","otherReason":""}}]'.format(adId),
-                'ca.kijiji.xsrf.token': getToken(myAdsPage.text, 'ca.kijiji.xsrf.token')
-                }
-        resp = self.session.post('https://www.kijiji.ca/j-delete-ad.json', data = params)
+            'Action': 'DELETE_ADS',
+            'Mode': 'ACTIVE',
+            'needsRedirect': 'false',
+            'ads': '[{{"adId":"{}","reason":"PREFER_NOT_TO_SAY","otherReason":""}}]'.format(adId),
+            'ca.kijiji.xsrf.token': getToken(myAdsPage.text, 'ca.kijiji.xsrf.token')
+            }
+        resp = self.session.post('https://www.kijiji.ca/j-delete-ad.json', data=params)
         if ("OK" not in resp.text):
             raise DeleteAdException(resp.text)
 
     def deleteAdUsingTitle(self, title):
         allAds = self.getAllAds()
         [self.deleteAd(i) for t, i in allAds if t.strip() == title.strip()]
-        
+
     #Allow user to pass in photos (as a array of string) they want to upload
     #Upload images concurrently using Pool
     def uploadImage(self, imageFiles=[]):
@@ -119,8 +116,8 @@ class KijijiApi:
         with Pool(5) as p:
             images = p.map(uploadOneImage, imageFiles)
         return [image for image in images if image is not None]
-    
-    #Data is a Dictionary that represents the data that will be posted to Kijijiserver
+
+    #Data is a dictionary that represents the data that will be posted to Kijiji
     #imageFiles represents a bunch of opened files (in string format) that need to be uploaded 
     def postAdUsingData(self, data, imageFiles=[]):
         #Upload the images
@@ -131,20 +128,18 @@ class KijijiApi:
         resp = self.session.get('https://www.kijiji.ca/p-admarkt-post-ad.html?categoryId=773')
         
         #Retrive tokens for website
-        xsrfToken = getToken(resp.text, 'ca.kijiji.xsrf.token') 
-        fraudToken = getToken(resp.text, 'postAdForm.fraudToken')
-        data['ca.kijiji.xsrf.token']=xsrfToken
-        data['postAdForm.fraudToken']=fraudToken
-
+        data['ca.kijiji.xsrf.token'] = getToken(resp.text, 'ca.kijiji.xsrf.token')
+        data['postAdForm.fraudToken'] = getToken(resp.text, 'postAdForm.fraudToken')
+        
         #Upload the ad itself
-        newAdUrl="https://www.kijiji.ca/p-submit-ad.html"
+        newAdUrl = "https://www.kijiji.ca/p-submit-ad.html"
         resp = self.session.post(newAdUrl, data=data)
         if not len(data.get("postAdForm.title", "")) >= 10:
             raise AssertionError("Your title is too short!")
         if (resp.status_code != 200 or \
                 "message-container success" not in resp.text):
             raise PostAdException(resp.text)
-
+        
         #Get adId and return it
         newCookieWithAdId = resp.headers['Set-Cookie']
         adId = re.search('\d+', newCookieWithAdId).group()
@@ -153,8 +148,7 @@ class KijijiApi:
     def getAllAds(self):
         myAdsUrl = 'http://www.kijiji.ca/j-get-my-ads.json'
         myAdsPage = self.session.get(myAdsUrl)
-        myAdsTree = json.loads(myAdsPage.text) 
+        myAdsTree = json.loads(myAdsPage.text)
         adIds = [entry['id'] for entry in myAdsTree['myAdEntries']]
         adNames = [entry['title'] for entry in myAdsTree['myAdEntries']]
         return zip(adNames, adIds)
-
