2015f6a1cfd4341981ec717ed12a30d436f268bf
jackm <jack@ehelion.com>
2017-06-24 18:40:53 -0400

    Added function documentation and cleaned up spacing

--- kijiji_repost_headless/kijiji_api.py
+++ kijiji_repost_headless/kijiji_api.py
@@ -9,7 +9,11 @@ from multiprocessing import Pool
 if sys.version_info < (3, 0):
     raise Exception("This program requires Python 3.0 or greater")
 
+
 class KijijiApiException(Exception):
+    """
+    Custom KijijiApi exception class
+    """
     def __init__(self, dump=None):
         self.dumpfilepath = ""
         if dump:
@@ -35,9 +39,11 @@ class DeleteAdException(KijijiApiException):
         return "Could not delete ad.\n"+super().__str__()
 
 
-#Retrive CSRF token from webpage
-#Tokens are different every time a page is visitied. 
 def get_token(html, tokenName):
+    """
+    Retrive CSRF token from webpage
+    Tokens are different every time a page is visitied
+    """
     soup = bs4.BeautifulSoup(html, 'html.parser')
     res = soup.select("[name={}]".format(tokenName))
     if not res:
@@ -46,7 +52,10 @@ def get_token(html, tokenName):
     return res[0]['value']
 
 def upload_one_image(imgFile):
-    #Try three times to upload the file. If successful, return the url.
+    """
+    Try up to three times to upload the file
+    If successful, return the url
+    """
     imageUploadUrl = 'https://www.kijiji.ca/p-upload-image.html'
     for i in range(0, 3):
         files = {'file': imgFile}
@@ -66,20 +75,17 @@ def upload_one_image(imgFile):
 
 
 class KijijiApi:
-    #login:  user, password -> None (Sets session to logged in)
-    #is_logged_in: None -> bool
-    #All function requires a logged in session to function correctly
-    #logout: None -> None
-    #post_ad: PostingFile -> adId
-    #delete_ad: adId -> None
-    #delete_ad_using_title: adTitle -> None
-    #get_all_ads: None -> list(vector(adname, adId))
-
+    """
+    All functions require to be logged in to Kijiji first in order to function correctly
+    """
     def __init__(self):
         config = {}
         self.session = requests.Session()
 
     def login(self, username, password):
+        """
+        Login to Kijiji for the current session
+        """
         loginUrl = 'https://www.kijiji.ca/t-login.html'
         resp = self.session.get(loginUrl)
         payload = {
@@ -95,13 +101,22 @@ class KijijiApi:
             raise SignInException(resp.text)
 
     def is_logged_in(self):
+        """
+        Return true if logged into Kijiji for the current session
+        """
         indexPageText = self.session.get('https://www.kijiji.ca/m-my-ads.html/').text
-        return "Sign Out" in indexPageText 
+        return "Sign Out" in indexPageText
 
     def logout(self):
+        """
+        Logout of Kijiji for the current session
+        """
         self.session.get('https://www.kijiji.ca/m-logout.html')
 
     def delete_ad(self, adId):
+        """
+        Delete ad based on ad ID
+        """
         myAdsPage = self.session.get('https://www.kijiji.ca/m-my-ads.html')
         params = {
             'Action': 'DELETE_ADS',
@@ -115,32 +130,42 @@ class KijijiApi:
             raise DeleteAdException(resp.text)
 
     def delete_ad_using_title(self, title):
+        """
+        Delete ad based on ad title
+        """
         allAds = self.get_all_ads()
         [self.delete_ad(i) for t, i in allAds if t.strip() == title.strip()]
 
-    #Allow user to pass in photos (as a array of string) they want to upload
-    #Upload images concurrently using Pool
     def upload_image(self, imageFiles=[]):
+        """
+        Upload one or more photos to Kijiji concurrently using Pool
+
+        'imageFiles' is a list of strings corresponding to image filenames
+        """
         images = []
         with Pool(5) as p:
             images = p.map(upload_one_image, imageFiles)
         return [image for image in images if image is not None]
 
-    #Data is a dictionary that represents the data that will be posted to Kijiji
-    #imageFiles represents a bunch of opened files (in string format) that need to be uploaded 
     def post_ad_using_data(self, data, imageFiles=[]):
-        #Upload the images
+        """
+        Post new ad
+
+        'data' is a dictionary of ad data that to be posted
+        'imageFiles' is a list of strings corresponding to image filenames to upload
+        """
+        # Upload the images
         imageList = self.upload_image(imageFiles)
         data['images'] = ",".join(imageList)
-        
-        #Load ad posting page
+
+        # Load ad posting page
         resp = self.session.get('https://www.kijiji.ca/p-admarkt-post-ad.html?categoryId=773')
-        
-        #Retrive tokens for website
+
+        # Retrive tokens for website
         data['ca.kijiji.xsrf.token'] = get_token(resp.text, 'ca.kijiji.xsrf.token')
         data['postAdForm.fraudToken'] = get_token(resp.text, 'postAdForm.fraudToken')
-        
-        #Upload the ad itself
+
+        # Upload the ad itself
         newAdUrl = "https://www.kijiji.ca/p-submit-ad.html"
         resp = self.session.post(newAdUrl, data=data)
         if not len(data.get("postAdForm.title", "")) >= 10:
@@ -148,13 +173,16 @@ class KijijiApi:
         if (resp.status_code != 200 or \
                 "message-container success" not in resp.text):
             raise PostAdException(resp.text)
-        
-        #Get adId and return it
+
+        # Get adId and return it
         newCookieWithAdId = resp.headers['Set-Cookie']
         adId = re.search('\d+', newCookieWithAdId).group()
         return adId
 
     def get_all_ads(self):
+        """
+        Return an iterator of tuples containing the ad title and ad ID for every ad
+        """
         resp = self.session.get('https://www.kijiji.ca/m-my-ads.html')
         userId=get_token(resp.text, 'userId')
         myAdsUrl = 'https://www.kijiji.ca/j-get-my-ads.json?_=1&currentOffset=0&isPromoting=false&show=ACTIVE&user={}'.format(userId)
--- kijiji_repost_headless/kijiji_cmd.py
+++ kijiji_repost_headless/kijiji_cmd.py
@@ -8,105 +8,122 @@ import kijiji_api
 if sys.version_info < (3, 0):
     raise Exception("This program requires Python 3.0 or greater")
 
+
 def main():
-   ##Start here 
-   #Takes: config(user/pass)
-   #One of:
-   #post adPostingFile
-   #show
-   #delete adId
-   #show
-   #repost adPostingFile
-   parser = argparse.ArgumentParser(
-           description="Post ads on Kijiji")
-   parser.add_argument('-u', '--username', help='username of your kijiji account')
-   parser.add_argument('-p', '--password', help='password of your kijiji account')
+    parser = argparse.ArgumentParser(
+            description="Post ads on Kijiji")
+    parser.add_argument('-u', '--username', help='username of your kijiji account')
+    parser.add_argument('-p', '--password', help='password of your kijiji account')
 
-   subparsers = parser.add_subparsers(help ='sub-command help')
-   
-   postParser = subparsers.add_parser('post', help='post a new ad')
-   postParser.add_argument('inf_file', type=str, help='.inf file containing posting details')
-   postParser.set_defaults(function=post_ad)
+    subparsers = parser.add_subparsers(help='sub-command help')
 
-   folderParser = subparsers.add_parser('folder', help='post ad from folder')
-   folderParser.add_argument('folderName', type=str, help='folder containing ad details')
-   folderParser.set_defaults(function=post_folder)
+    postParser = subparsers.add_parser('post', help='post a new ad')
+    postParser.add_argument('inf_file', type=str, help='.inf file containing posting details')
+    postParser.set_defaults(function=post_ad)
 
-   repostFolderParser = subparsers.add_parser('repost_folder', help='post ad from folder')
-   repostFolderParser.add_argument('folderName', type=str, help='folder containing ad details')
-   repostFolderParser.set_defaults(function=repost_folder)
-   
-   showParser = subparsers.add_parser('show', help='show currently listed ads')
-   showParser.set_defaults(function=show_ads)
+    folderParser = subparsers.add_parser('folder', help='post ad from folder')
+    folderParser.add_argument('folderName', type=str, help='folder containing ad details')
+    folderParser.set_defaults(function=post_folder)
 
-   deleteParser = subparsers.add_parser('delete', help='delete a listed ad')
-   deleteParser.add_argument('id',type=str, help='id of the ad you wish to delete')
-   deleteParser.set_defaults(function=delete_ad)
+    repostFolderParser = subparsers.add_parser('repost_folder', help='post ad from folder')
+    repostFolderParser.add_argument('folderName', type=str, help='folder containing ad details')
+    repostFolderParser.set_defaults(function=repost_folder)
 
-   nukeParser = subparsers.add_parser('nuke', help='delete all ads')
-   nukeParser.set_defaults(function=nuke)
+    showParser = subparsers.add_parser('show', help='show currently listed ads')
+    showParser.set_defaults(function=show_ads)
 
-   repostParser = subparsers.add_parser('repost', help='repost an existing ad')
-   repostParser.add_argument('inf_file', type = str,help = '.inf file containing posting details')
-   repostParser.set_defaults(function=repost_ad)
+    deleteParser = subparsers.add_parser('delete', help='delete a listed ad')
+    deleteParser.add_argument('id',type=str, help='id of the ad you wish to delete')
+    deleteParser.set_defaults(function=delete_ad)
 
-   args = parser.parse_args()
-   #try:
-   args.function(args)
-   #except AttributeError as err:
-    #   print(err)
-    #   parser.print_help()
+    nukeParser = subparsers.add_parser('nuke', help='delete all ads')
+    nukeParser.set_defaults(function=nuke)
 
-#HELPER FUNCTIONS
+    repostParser = subparsers.add_parser('repost', help='repost an existing ad')
+    repostParser.add_argument('inf_file', type=str, help='.inf file containing posting details')
+    repostParser.set_defaults(function=repost_ad)
+
+    args = parser.parse_args()
+    #try:
+    args.function(args)
+    #except AttributeError as err:
+    #    print(err)
+    #    parser.print_help()
+
+
 def get_folder_data(args):
+    """
+    Set ad data inf file and extract login credentials from inf files
+    """
     args.inf_file = "item.inf"
     cred_file = args.folderName+"/login.inf"
     f = open(cred_file, 'r')
     creds = [line.strip() for line in f]
-    args.username = creds[0] 
-    args.password = creds[1] 
+    args.username = creds[0]
+    args.password = creds[1]
 
 def get_inf_details(inf_file):
+    """
+    Extract ad data from inf file
+    """
     with open(inf_file, 'rt') as infFileLines:
         data = {key: val for line in infFileLines for (key, val) in (line.strip().split("="),)}
     files = [open(picture, 'rb').read() for picture in data['imageCsv'].split(",")]
     return [data, files]
 
-##Actual Functions called from main
+
 def post_folder(args):
+    """
+    Post new ad from folder
+    """
     get_folder_data(args)
     os.chdir(args.folderName)
     post_ad(args)
 
 def post_ad(args):
+    """
+    Post new ad
+    """
     [data, imageFiles] = get_inf_details(args.inf_file)
     api = kijiji_api.KijijiApi()
     api.login(args.username, args.password)
     api.post_ad_using_data(data, imageFiles)
 
 def show_ads(args):
+    """
+    Print list of all ads
+    """
     api = kijiji_api.KijijiApi()
     api.login(args.username, args.password)
     [print("{} '{}'".format(adId, adName)) for adName, adId in api.get_all_ads()]
 
 def delete_ad(args):
+    """
+    Delete ad
+    """
     api = kijiji_api.KijijiApi()
     api.login(args.username, args.password)
     api.delete_ad(args.id)
 
 def delete_ad_using_title(name):
+    """
+    Delete ad based on ad title
+    """
     api = kijiji_api.KijijiApi()
     api.delete_ad_using_title(name)
 
-#Try to delete ad with same name if possible
-#post new ad
 def repost_ad(args):
+    """
+    Repost ad
+    
+    Try to delete ad with same title if possible before reposting new ad
+    """
     api = kijiji_api.KijijiApi()
     api.login(args.username, args.password)
     delAdName = ""
     for line in open(args.inf_file, 'rt'):
         [key, val] = line.strip().rstrip("\n").split("=")
-        if key =='postAdForm.title':
+        if key == "postAdForm.title":
             delAdName = val
     try:
         api.delete_ad_using_title(delAdName)
@@ -119,16 +136,22 @@ def repost_ad(args):
     post_ad(args)
 
 def repost_folder(args):
+    """
+    Repost ad from folder
+    """
     get_folder_data(args)
     os.chdir(args.folderName)
     repost_ad(args)
 
 def nuke(args):
+    """
+    Delete all ads
+    """
     api = kijiji_api.KijijiApi()
     api.login(args.username, args.password)
     allAds = api.get_all_ads()
     [api.delete_ad(adId) for adName, adId in allAds]
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     main()
