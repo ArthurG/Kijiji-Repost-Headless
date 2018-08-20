09513e060bb02e06423c0db22e744c74ab87ab5b
jordanmehravar <mehravarj@gmail.com>
2018-08-20 14:24:26 -0400

    Changed title character minimum from 10 to 8

--- kijiji_repost_headless/kijiji_api.py
+++ kijiji_repost_headless/kijiji_api.py
@@ -185,8 +185,8 @@ class KijijiApi:
         # Format ad data and check constraints
         data['postAdForm.description'] = data['postAdForm.description'].replace("\\n", "\n")
         title_len = len(data.get("postAdForm.title", ""))
-        if not title_len >= 10:
-            raise KijijiApiException("Your ad title is too short! (min 10 chars)")
+        if not title_len >= 8:
+            raise KijijiApiException("Your ad title is too short! (min 8 chars)")
         if title_len > 64:
             raise KijijiApiException("Your ad title is too long! (max 64 chars)")
 
